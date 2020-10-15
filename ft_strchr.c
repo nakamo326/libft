6637155b7f3a81ad4298b9b00ac2c4dc85c324f5
Yasufumi Nakamoto <ynakamot@student.42tokyo.jp>
2020-10-16 01:11:10 +0900

    fix strchr

--- ft_strchr.c
+++ ft_strchr.c
@@ -6,7 +6,7 @@
 /*   By: ynakamot <ynakamot@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/07 23:26:20 by ynakamot          #+#    #+#             */
-/*   Updated: 2020/10/08 13:43:44 by ynakamot         ###   ########.fr       */
+/*   Updated: 2020/10/16 00:52:02 by ynakamot         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,18 +14,16 @@
 
 char	*ft_strchr(const char *s, int c)
 {
-	char			*r;
-	unsigned char	chr;
+	char	*r;
 
 	r = (char *)s;
-	chr = (unsigned char)c;
 	while (*r)
 	{
 		if (*r == c)
 			return (r);
 		r++;
 	}
-	if (chr == '\0')
+	if (c == '\0')
 		return (r);
 	else
 		return (NULL);
