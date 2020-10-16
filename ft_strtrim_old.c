6ee38bf4118678516264e9c14d614be1d62f4f5e
Yasufumi Nakamoto <ynakamot@student.42tokyo.jp>
2020-10-16 10:39:07 +0900

    fix strtrim

--- ft_strtrim_old.c
+++ ft_strtrim_old.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strtrim copy.c                                  :+:      :+:    :+:   */
+/*   ft_strtrim_old.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ynakamot <ynakamot@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/10 10:17:53 by ynakamot          #+#    #+#             */
-/*   Updated: 2020/10/16 10:38:25 by ynakamot         ###   ########.fr       */
+/*   Updated: 2020/10/16 10:38:52 by ynakamot         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,21 +23,37 @@ static int	isset(char c, char const *set)
 	return (0);
 }
 
+static int	count(char const *s1, char const *set)
+{
+	int len;
+
+	len = 0;
+	while (isset(*s1, set))
+		s1++;
+	if (*s1 == '\0')
+		return (len);
+	len = ft_strlen(s1) - 1;
+	while (isset(s1[len], set))
+		len--;
+	return (len + 1);
+}
+
 char		*ft_strtrim(char const *s1, char const *set)
 {
 	char	*str;
-	size_t	len;
-	size_t	i;
+	int		len;
+	int		i;
 
 	if (!s1 || !set)
 		return (NULL);
 	i = 0;
-	while (isset(s1[i], set))
-		i++;
-	len = ft_strlen(s1[i]);
-	while(isset(s1[i + len - 1], set))
-		len--;
-	if(!(str = ft_substr(s1, (unsigned int)i, len)))
+	len = count(s1, set);
+	if (!(str = malloc(sizeof(char) * (len + 1))))
 		return (NULL);
+	while (isset(*s1, set))
+		s1++;
+	while (i < len)
+		str[i++] = *s1++;
+	str[i] = '\0';
 	return (str);
 }
