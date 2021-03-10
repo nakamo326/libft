b7adc89e880485c64a35a2c2acc03b4dd1ea907a
Yasufumi Nakamoto <nakamo326@gmail.com>
2021-03-10 12:21:16 +0900

    add null guard to free_split

--- ft_free_split.c
+++ ft_free_split.c
@@ -6,7 +6,7 @@
 /*   By: ynakamot <ynakamot@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/30 21:35:23 by ynakamot          #+#    #+#             */
-/*   Updated: 2021/01/19 22:00:44 by ynakamot         ###   ########.fr       */
+/*   Updated: 2021/03/10 12:20:38 by ynakamot         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,8 @@ void	ft_free_split(char **strs)
 	int i;
 
 	i = 0;
+	if (!str)
+		return ;
 	while (strs[i] != NULL)
 		i++;
 	while (i >= 0)
