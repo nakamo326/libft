ee935537069edc62de6d5fc33fc7eb33dfdc0f9c
Yasufumi Nakamoto <nakamo326@gmail.com>
2021-03-19 10:39:45 +0900

    add null guard to strdup

--- ft_strdup.c
+++ ft_strdup.c
@@ -6,7 +6,7 @@
 /*   By: ynakamot <ynakamot@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/08/18 10:49:34 by ynakamot          #+#    #+#             */
-/*   Updated: 2020/10/13 00:58:02 by ynakamot         ###   ########.fr       */
+/*   Updated: 2021/03/19 10:38:21 by ynakamot         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,8 @@ char	*ft_strdup(const char *src)
 	char	*str;
 	size_t	len;
 
+	if (src == NULL)
+		return (NULL);
 	len = ft_strlen(src);
 	if (!(str = malloc(sizeof(*str) * (len + 1))))
 		return (NULL);
