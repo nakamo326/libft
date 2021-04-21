fd6c057a01997b608e1ba35d331a6c95abfb3bee
Yasufumi Nakamoto <nakamo326@gmail.com>
2021-04-22 01:12:04 +0900

    add malloc error handling

--- ft_addstr_split.c
+++ ft_addstr_split.c
@@ -8,17 +8,25 @@ char	**ft_addstr_split(char **list, const char *str)
 {
 	size_t	i;
 	char	*new_str;
+	char	**tmp;
 
 	new_str = ft_strdup(str);
 	if (new_str == NULL)
-		return (list);
+	{
+		ft_free_split(list);
+		return (NULL);
+	}
 	i = 0;
 	if (list != NULL)
 		while (list[i] != NULL)
 			i++;
+	tmp = list;
 	list = ft_extend_split(list, i + 2);
 	if (list == NULL)
+	{
+		ft_free_split(tmp);
 		return (NULL);
+	}
 	list[i] = new_str;
 	list[i + 1] = NULL;
 	return (list);
